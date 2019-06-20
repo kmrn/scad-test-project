ca89d259a47c053603379a108e014d8290526eb5
kmrn <hello@kamranpayne.com>
2019-06-20 01:07:22 -0400

    add custom validating and use strict mode

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
+(function () {
+    'use strict';
 
-$('#contactForm').submit(function (event) {
-    event.preventDefault();
-
-    var formData = {
-        "name": $('#inputName').val(),
-        "email": $('#inputEmail').val(),
-        "message": $('#textareaMessage').val()
-    };
-    var xhr = new XMLHttpRequest();
-    xhr.onloadend = function () {
-        if (xhr.status === 200) {
-            console.log("success");
-        } else {
-            console.log("NOPE");
+    $('#contactForm').submit(function (event) {
+        var self = this; 
+        event.preventDefault();
+        if (self.checkValidity() === false) {
+            event.stopPropagation();
+            self.classList.add('was-validated');
         }
-    };
+        else {
+            var formData = {
+                "name": $('#inputName').val(),
+                "email": $('#inputEmail').val(),
+                "message": $('#textareaMessage').val()
+            };
+            var xhr = new XMLHttpRequest();
+    
+            xhr.open('POST', 'contactForm.php', true);
+            xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-    xhr.open('POST', 'contactForm.php', true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.send(JSON.stringify(formData));
-});
+            xhr.onloadend = function () {
+                if (xhr.status === 200) {
+                    self.classList.remove('was-validated');
+                    self.reset();
+                    $('#contactSubmitButton').popover({
+                        content: "Your message was sent! Thank you for contacting us.",
+                        trigger: "manual",
+                        placement: "top"
+                    }).popover('show');
+                } else {
+                    $('#contactSubmitButton').popover({
+                        content: "There was an error sending your message. Contact us directly at web@scad.edu.",
+                        trigger: "manual",
+                        placement: "top"
+                    }).popover('show');
+                }
+
+                setTimeout(function () { 
+                    $('#contactSubmitButton').popover('hide');
+                }, 4000);
+            };
+
+            xhr.send(JSON.stringify(formData));
+        }
+    });
+})();
--- js/signups.js
+++ js/signups.js
@@ -1,33 +1,36 @@
-
-function getSignups() {
-    var xhr = new XMLHttpRequest();
-    xhr.onloadend = function () {
-        if (xhr.status !== 200) {
-            console.error("error getting signups - " + xhr.status);
-        }
-    };
-
-    xhr.open('GET', 'getSignups.php', true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var signups = JSON.parse(xhr.responseText).body;
-            var tableBody = "";
-            for (var i = 0; i < signups.length; i++) {
-                tableBody += "<tr>" +
-                            "   <td>" + signups[i].SID + "</td>" +
-                            "   <td>" + signups[i].name + "</td>" +
-                            "   <td>" + signups[i].email + "</td>" +
-                            "   <td>" + signups[i].created + "</td>" +
-                            "</tr>";
+(function () {
+    'use strict';
+    
+    function getSignups() {
+        var xhr = new XMLHttpRequest();
+        xhr.onloadend = function () {
+            if (xhr.status !== 200) {
+                console.error("error getting signups - " + xhr.status);
             }
+        };
 
-            $('#signupsTableBody').html(tableBody);
-        }
-    };
-    xhr.send();
-}
+        xhr.open('GET', 'getSignups.php', true);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var signups = JSON.parse(xhr.responseText).body;
+                var tableBody = "";
+                for (var i = 0; i < signups.length; i++) {
+                    tableBody += "<tr>" +
+                                "   <td>" + signups[i].SID + "</td>" +
+                                "   <td>" + signups[i].name + "</td>" +
+                                "   <td>" + signups[i].email + "</td>" +
+                                "   <td>" + signups[i].created + "</td>" +
+                                "</tr>";
+                }
 
-getSignups();
+                $('#signupsTableBody').html(tableBody);
+            }
+        };
+        xhr.send();
+    }
 
-$('#refreshButton').click(getSignups);
+    getSignups();
+
+    $('#refreshButton').click(getSignups);
+})();
