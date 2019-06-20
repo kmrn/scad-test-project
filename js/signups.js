750f62ac779f23dbc2e94b4ebd8f520486dac4ba
kmrn <hello@kamranpayne.com>
2019-06-19 23:17:52 -0400

    add refresh button and header

--- js/signups.js
+++ js/signups.js
@@ -1,26 +1,33 @@
-var xhr = new XMLHttpRequest();
-xhr.onloadend = function () {
-    if (xhr.status !== 200) {
-        console.error("error getting signups - " + xhr.status);
-    }
-};
 
-xhr.open('GET', 'getSignups.php', true);
-xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        var signups = JSON.parse(xhr.responseText).body;
-        var tableBody = "";
-        for (var i = 0; i < signups.length; i++) {
-            tableBody += "<tr>" +
-                        "   <td>" + signups[i].SID + "</td>" +
-                        "   <td>" + signups[i].name + "</td>" +
-                        "   <td>" + signups[i].email + "</td>" +
-                        "   <td>" + signups[i].created + "</td>" +
-                        "</tr>";
+function getSignups() {
+    var xhr = new XMLHttpRequest();
+    xhr.onloadend = function () {
+        if (xhr.status !== 200) {
+            console.error("error getting signups - " + xhr.status);
         }
+    };
 
-        $('#signupsTableBody').html(tableBody);
-    }
-};
-xhr.send();
+    xhr.open('GET', 'getSignups.php', true);
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var signups = JSON.parse(xhr.responseText).body;
+            var tableBody = "";
+            for (var i = 0; i < signups.length; i++) {
+                tableBody += "<tr>" +
+                            "   <td>" + signups[i].SID + "</td>" +
+                            "   <td>" + signups[i].name + "</td>" +
+                            "   <td>" + signups[i].email + "</td>" +
+                            "   <td>" + signups[i].created + "</td>" +
+                            "</tr>";
+            }
+
+            $('#signupsTableBody').html(tableBody);
+        }
+    };
+    xhr.send();
+}
+
+getSignups();
+
+$('#refreshButton').click(getSignups);
